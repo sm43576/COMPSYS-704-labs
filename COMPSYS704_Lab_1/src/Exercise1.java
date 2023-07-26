05c5b0c8d47c5a9900b54cf6e643a80715cc7df7
sm43576 <79734318+sm43576@users.noreply.github.com>
2023-07-26 12:43:32 +1200

    Finished ex1

--- COMPSYS704_Lab_1/src/Exercise1.java
+++ COMPSYS704_Lab_1/src/Exercise1.java
@@ -3,13 +3,22 @@ public class Exercise1 {
 	// The main function is the entry point for execution
 	public static void main(String[] args) {
 		int x = 20;
-		int y = 22;
+		int y = 15;
 
 		// TODO: Replace the ellipses
-		System.out.println(...);
-		System.out.println(... + "!");
-		System.out.println("The year is: " + ...);
-		System.out.println("The result is: " + ...);
-		System.out.println("The result is: " + ...);
+		System.out.println(x+y);
+		System.out.println(String.valueOf(x+y) + "!");
+		System.out.println("The year is: " + String.valueOf(x) + String.valueOf(y));
+		System.out.println("The result is: " + String.valueOf(x+y) );
+		System.out.println("The result is: " + String.valueOf(x*y) );
 	}
 }
+
+
+/* 
+35
+35!
+The year is: 2015
+The result is: 35
+The result is: 300
+*/
