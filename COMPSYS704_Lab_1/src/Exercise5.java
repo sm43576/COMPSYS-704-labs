54ba0e2e592adc1eb36177270180eeb7d4a8a245
sm43576 <79734318+sm43576@users.noreply.github.com>
2023-07-27 10:30:33 +1200

    Ex5 done

--- COMPSYS704_Lab_1/src/Exercise5.java
+++ COMPSYS704_Lab_1/src/Exercise5.java
@@ -1,18 +1,28 @@
-import Exercise2.Fibonacci;
 
 public class Exercise5 {
 	
 	public static void main(String[] args) {
 
 		unoReverse number = new unoReverse(1938);
+		number.reverseString();
 	}
 	
 	static class unoReverse {
-		private int n;
+		private String n;
 
 		public unoReverse(int n) {
-			this.n = n;
+			this.n = String.valueOf(n);
 		}//https://canvas.auckland.ac.nz/courses/86782/files/11266247?module_item_id=1906835#
 
+		public void reverseString() {
+			String originalStr = this.n;
+			String reversedStr = "";	
+			
+			for (int i = 0; i < originalStr.length(); i++) {
+				  reversedStr = originalStr.charAt(i) + reversedStr;
+				}
 
+				System.out.println("Reversed string: "+ reversedStr);
+		}
+}
 }
