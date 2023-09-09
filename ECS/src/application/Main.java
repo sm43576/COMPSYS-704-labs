e34839066e44781beb0b0f2f1edbb892e35107f6
sm43576 <79734318+sm43576@users.noreply.github.com>
2023-09-09 17:55:50 +1200

    sorted out GUI

--- ECS/src/application/Main.java
+++ ECS/src/application/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application{
 
     public static void main(String[] args){
@@ -21,10 +20,13 @@ public class Main extends Application{
         ecsUI controller = loader.getController();
         
         primaryStage.setTitle("ECS Window");
-        primaryStage.setScene(new Scene(root,520,350));
+        primaryStage.setScene(new Scene(root,850,520));
         primaryStage.show();
         
         controller.setZoneCircle("zone1circle",1);
+
+        controller.setHumidity("humid3", 20);
+        controller.setTemp("temp1",24);
        
         //scene.getStyleSheets().add(getClass().getResource("application.css"));
     	} catch (Exception e) {
